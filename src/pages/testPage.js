3327960db584ff4b9f1e7506b44fb0fc273f9d11
guruprasadlakkond <guruprasadlakkond@gmail.com>
2018-04-05 15:46:12 +0530

    updated page

--- src/pages/testPage.js
+++ src/pages/testPage.js
@@ -5,6 +5,7 @@ const Mypage = () => (
   <div>
     <h1>This is test page</h1>
     <p>Welcome to Test Page</p>
+    <p>Page has been updated</p>
     <Link to="/">Go back to the homepage</Link>
   </div>
 )
